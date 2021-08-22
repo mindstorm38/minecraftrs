33518a1de4ca18ce5d0679639f063f5e28f9dd70
Theo Rozier <contact@theorozier.fr>
2021-08-22 15:19:56 +0200

    Removed useless code un runtime example.

--- mc/runtime/examples/run.rs
+++ mc/runtime/examples/run.rs
@@ -16,8 +16,6 @@ fn main() {
 
 fn register_systems(world: &mut World, executor: &mut WorldSystemExecutor) {
 
-    // world.insert_component(TpsTrackerComponent::new());
-    // executor.add_system(debug_tps);
     executor.add_system(system_load_chunks);
 
     println!("Systems");
@@ -26,52 +24,3 @@ fn register_systems(world: &mut World, executor: &mut WorldSystemExecutor) {
     }
 
 }
-
-
-/*fn debug_tps(world: &mut World) {
-    let mut tps_tracker = world.get_component_mut::<TpsTrackerComponent>().unwrap();
-    println!("TPS: {}", tps_tracker.tick());
-}
-
-
-pub struct TpsTrackerComponent {
-    last_time: Instant,
-    tps_sum: f32,
-    tps_count: usize,
-    tps: f32
-}
-
-impl TpsTrackerComponent {
-
-    pub fn new() -> Self {
-        Self {
-            last_time: Instant::now(),
-            tps_sum: 0.0,
-            tps_count: 0,
-            tps: 0.0
-        }
-    }
-
-    pub fn tick(&mut self) -> f32 {
-
-        let now = Instant::now();
-        let duration = now - self.last_time;
-        let tps = 1.0 / duration.as_secs_f32();
-
-        self.last_time = now;
-
-        self.tps_sum += tps;
-        self.tps_count += 1;
-
-        if self.tps_count == 20 {
-            self.tps = self.tps_sum / 20.0;
-            self.tps_sum = 0.0;
-            self.tps_count = 0;
-        }
-
-        self.tps
-
-    }
-
-}
-*/
