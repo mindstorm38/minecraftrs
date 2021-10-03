8ef00fedbd353353728e8645d11d5aaf406d06bf
Theo Rozier <contact@theorozier.fr>
2021-10-03 20:06:08 +0200

    Improved (should have been) the EventTracker from mc-runtime. To test.

--- mc/runtime/src/util/event.rs
+++ mc/runtime/src/util/event.rs
@@ -1,5 +1,7 @@
 use std::any::{TypeId, Any};
 use std::ops::{Deref, DerefMut};
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 
 struct Event {
@@ -10,44 +12,68 @@ struct Event {
 
 
 pub struct EventTracker {
-    events: Vec<Event>
+    events: HashMap<TypeId, Vec<Event>>,
+    empty_vec_pool: Vec<Vec<Event>>
 }
 
 impl EventTracker {
 
     pub fn new() -> Self {
         Self {
-            events: Vec::new()
+            events: HashMap::new(),
+            empty_vec_pool: Vec::new()
         }
     }
 
     pub fn push_event<T: Any>(&mut self, event: T) {
-        self.events.push(Event {
+        match self.events.entry(TypeId::of::<T>()) {
+            Entry::Occupied(mut o) => o.get_mut(),
+            Entry::Vacant(v) => {
+                v.insert(self.empty_vec_pool.pop().unwrap_or_else(|| Vec::new()))
+            }
+        }.push(event)
+
+        /*self.events.push(Event {
             event: Box::new(event),
             tid: TypeId::of::<T>(),
             invalid: false
-        })
+        })*/
     }
 
     /// Return true if the tracker has any pending event of the given type `T`.
     pub fn has_event<T: Any>(&self) -> bool {
-        self.events.iter().any(|event| !event.invalid && event.tid == TypeId::of::<T>())
+        return self.events.contains_key(&TypeId::of::<T>())
+        // self.events.iter().any(|event| !event.invalid && event.tid == TypeId::of::<T>())
     }
 
     /// Simple iteration over all events of the same type.
     pub fn poll_events<'a, T: Any>(&'a self) -> impl Iterator<Item = &'a T> + 'a {
-        self.events.iter()
+        match self.events.get(&TypeId::of::<T>()) {
+            Some(events) => events.iter().filter(|event| !event.invalid),
+            None => std::iter::empty()
+        }
+        /*self.events.iter()
             .filter(|event| !event.invalid && event.tid == TypeId::of::<T>())
             .map(|event| {
                 // SAFETY: We can unwrap because the TypeId is checked in the filter.
                 event.event.downcast_ref().unwrap()
-            })
+            })*/
     }
 
     /// Simple iteration over all events handles of the same type, this allows
     /// mutating events and cancelling them.
     pub fn poll_events_handles<'a, T: Any>(&'a mut self) -> impl Iterator<Item = EventHandle<'a, T>> + 'a {
-        self.events.iter_mut()
+        match self.events.get_mut(&TypeId::of::<T>()) {
+            Some(events) => events.iter_mut().map(|event| {
+                // SAFETY: We can unwrap because the TypeId is checked in the filter.
+                EventHandle {
+                    event: event.event.downcast_mut().unwrap(),
+                    invalid: &mut event.invalid
+                }
+            }),
+            None => std::iter::empty()
+        }
+        /*self.events.iter_mut()
             .filter(|event| !event.invalid && event.tid == TypeId::of::<T>())
             .map(|event| {
                 // SAFETY: We can unwrap because the TypeId is checked in the filter.
@@ -55,11 +81,12 @@ impl EventTracker {
                     event: event.event.downcast_mut().unwrap(),
                     invalid: &mut event.invalid
                 }
-            })
+            })*/
     }
 
     pub fn clear_events(&mut self) {
-        self.events.clear();
+        // self.events.clear();
+        self.empty_vec_pool.extend(self.events.drain());
     }
 
 }
